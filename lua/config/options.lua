b89884d17e93b1e6cf1e278d61d2117ee65845ed
test <test@test.com>
2024-12-30 22:23:45 +0800

    add plugins: neodim, leap

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,4 +6,4 @@ local opt = vim.opt
 
 opt.relativenumber = false
 
-opt.extc = true
+opt.exrc = true
---
+++ lua/plugins/custom.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "zbirenbaum/neodim",
+    lazy = true,
+    event = "LspAttach",
+    config = function()
+      require("neodim").setup({
+        alpha = 0.75,
+        blend_color = "#000000",
+        update_in_insert = {
+          enable = true,
+          delay = 100,
+        },
+        hide = {
+          virtual_text = true,
+          signs = false,
+          underline = false,
+        },
+      })
+    end,
+  },
+}
