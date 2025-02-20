9d1a383f4bd9eea4ff6af06e7e6211980203a85d
juyifs <160045693+juyifs@users.noreply.github.com>
2025-02-20 09:30:40 +0800

    Update git keymaps to auto find root dir

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,7 +11,7 @@ end, { desc = "go to definition" })
 
 map({ "n", "v" }, "<A-LeftMouse>", function()
   vim.cmd.exec('"normal! \\<LeftMouse>"')
-  require("telescope.builtin").lsp_references()
+  vim.lsp.buf.references()
 end, { desc = "find references" })
 
 map({ "n", "v" }, "<A-RightMouse>", function()
@@ -37,6 +37,38 @@ end
 
 map("v", "d", '"_d', { noremap = true, silent = true })
 
+map("n", "<leader>tt", ":enew | term<CR>", { noremap = true, silent = true })
+
 map("", "<leader>ss", require("telescope.builtin").lsp_document_symbols, { desc = "Goto Symbol" })
 
-map("", "<leader>gb", require("telescope.builtin").git_bcommits_range, { desc = "Git Line Log" })
+map("", "<leader>gb", function ()
+  require("telescope.builtin").git_bcommits_range(
+    {
+      cwd = vim.fn.finddir(".git/..", vim.fn.expand("%:p:h") .. ";"),
+    }
+  )
+end, { desc = "Git Line Log" })
+
+map("", "<leader>gs", function ()
+  require("telescope.builtin").git_status(
+    {
+      cwd = vim.fn.finddir(".git/..", vim.fn.expand("%:p:h") .. ";"),
+    }
+  )
+end , {desc = "Status"})
+map("", "<leader>gc", function ()
+  require("telescope.builtin").git_commits(
+    {
+      cwd = vim.fn.finddir(".git/..", vim.fn.expand("%:p:h") .. ";"),
+    }
+  )
+end, {desc = "Commits"} )
+
+-- lazygit
+if vim.fn.executable("lazygit") == 1 then
+  map("n", "<leader>gg", function() Snacks.lazygit( { cwd = vim.fn.finddir(".git/..", vim.fn.expand("%:p:h") .. ";") }) end, { desc = "Lazygit (Root Dir)" })
+  map("n", "<leader>gG", function() Snacks.lazygit({ cwd = vim.fn.expand("%:p:h") }) end, { desc = "Lazygit (cwd)" })
+  map("n", "<leader>gf", function() Snacks.picker.git_log_file() end, { desc = "Git Current File History" })
+  map("n", "<leader>gl", function() Snacks.picker.git_log( { cwd = vim.fn.finddir(".git/..", vim.fn.expand("%:p:h") .. ";") }) end, { desc = "Git Log" })
+  map("n", "<leader>gL", function() Snacks.picker.git_log({ cwd = vim.fn.expand("%:p:h") }) end, { desc = "Git Log (cwd)" })
+end
