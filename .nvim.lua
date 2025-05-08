5c05652f3a32556505b57c1f396810d9ad02c427
juyifs <160045693+juyifs@users.noreply.github.com>
2025-05-08 13:13:44 +0800

    Create .nvim.lua

---
+++ .nvim.lua
@@ -0,0 +1,38 @@
+local ctx = require("exrc").init()
+ctx:lsp_setup({
+	require("lspconfig").clangd.setup({
+		cmd = {
+			"clangd",
+			"--background-index",
+			"--clang-tidy",
+			"--header-insertion=iwyu",
+			"--completion-style=detailed",
+			"--function-arg-placeholders",
+			"--fallback-style=llvm",
+			"--query-driver=/opt/toolchain/gcc_*",
+		},
+	}),
+	require("telescope").setup({
+		pickers = {
+			find_files = {
+				search_dirs = { "/AA/BB", "/AA/CC", ".nvim.lua" },
+			},
+			live_grep = {
+				search_dirs = { "/AA/BB", "/AA/CC", ".nvim.lua" },
+			},
+			grep_string = {
+				search_dirs = { "/AA/BB", "/AA/CC", ".nvim.lua" },
+			},
+			lsp_dynamic_workspace_symbols = {
+				search_dirs = { "/AA/BB", "/AA/CC", ".nvim.lua" },
+			},
+		},
+    extensions = {
+      live_grep_args = {
+        search_dirs = {
+          "/AA/BB","/AA/CC"
+        },
+      }
+    },
+	}),
+})
