d2e84451363f7e817e1a406c76f8511a6e479757
juyifs <160045693+juyifs@users.noreply.github.com>
2025-12-25 13:27:58 +0800

    Add OSC 52 clipboard configuration
    Configure clipboard settings to use OSC 52 for copy and paste operations.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -29,3 +29,15 @@ vim.o.background = 'light'
 vim.g.root_spec = { { ".git", ".nvim.lua", "lua" }, "lsp", "cwd" }
 
 vim.g.ZFDirDiff_excludeFilter = { "*.o", "*.d", "*.a", "*.so" }
+
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
